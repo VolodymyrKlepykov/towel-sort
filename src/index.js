f50056993e59a2e8560c1521d92a23bbdf45e2cd
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-03-10 10:30:43 +0200

    fix: fix array is not array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  if(!matrix || matrix.length === 0){
-    return [];
-  }
-  return matrix.filter((a, b) => b % 2 === 0 ? a : a.reverse()).flat()
-}
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix) || matrix.length === 0) {
+        return [];
+    }
+    return matrix.filter((a, b) => (b % 2 === 0 ? a : a.reverse())).flat();
+};
